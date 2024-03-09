21e80130c19dc7764ddbe2551b6aed49b450626b
Samuel Joel T <samueljoel464@gmail.com>
2024-03-09 16:41:43 +0700

    feat: Adding Routing in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
   return (
     <div className="bg-background/95 sticky left-0 right-0 top-0 z-20 border-b-1 backdrop-blur">
       <nav className="container bg-black flex h-20 items-center justify-center px-4">
-        <img className="size-16" src="/icons8-pokedex-48.png" alt="pokedex-logo" />
+        <Link to="/">
+          <img
+            className="size-16"
+            src="/icons8-pokedex-48.png"
+            alt="pokedex-logo"
+          />
+        </Link>
       </nav>
     </div>
   );
