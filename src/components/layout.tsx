71fbcd2e981c5c0b6bcb2aeea447a5d34db2877b
Samuel Joel T <samueljoel464@gmail.com>
2024-03-09 14:21:57 +0700

    feat: some changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,11 +12,11 @@ const Layout = (props: Props) => {
 
   return (
     <div className="flex justify-center bg-slate-900">
-      <div className="layout-container min-w-full max-w-full bg-white md:min-w-[480px] md:max-w-[480px]">
+      <div className="layout-container min-w-full max-w-full bg-gray-400 md:min-w-[480px] md:max-w-[480px]">
         <Navbar />
         <div
           className={cn(
-            "w-full bg-blue-950 h-screen container grow py-4 px-8 flex flex-col",
+            "w-full h-screen container grow py-4 px-8 flex flex-col",
             centerX && "items-center",
             centerY && "justify-center"
           )}
