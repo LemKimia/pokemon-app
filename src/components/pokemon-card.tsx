6314006bcd0fe9ea66e4f5a8cfa860009bb08112
Samuel Joel T <samueljoel464@gmail.com>
2024-03-14 09:54:14 +0700

    fix: changes in card

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,26 +1,28 @@
-import { Card, CardContent } from "@/components/ui/card";
+
 import { IPokemon } from "@/utils/types/results";
 import { Link } from "react-router-dom";
 
 interface Props {
   pokemon: IPokemon;
-  image_url: string
+  image_url: string;
 }
 
 const PokemonCard = (props: Props) => {
   const { pokemon, image_url } = props;
 
   return (
-    <Card className="flex h-full flex-col rounded-2xl border-4 border-black shadow-lg shadow-black dark:border-white">
-      <CardContent className="flex h-full flex-col items-center justify-between p-0">
+    <div className="flex h-full flex-col rounded-2xl border-4 border-black shadow-lg shadow-black">
+      <div className="flex h-full flex-col items-center justify-between">
         <Link to={`/pokemon-details/${pokemon.name}`}>
-          <img src={image_url} alt={pokemon.name} />
+          <div className="flex h-full w-full items-center justify-center">
+            <img className="h-auto w-auto" src={image_url} alt={pokemon.name} />
+          </div>
         </Link>
         <p className="w-full rounded-b-lg bg-black py-2 text-center font-arcade text-xs font-bold uppercase tracking-widest text-white dark:rounded-b-xl">
           {pokemon.name}
         </p>
-      </CardContent>
-    </Card>
+      </div>
+    </div>
   );
 };
 
