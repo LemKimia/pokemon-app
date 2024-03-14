3f27adfa13c789be4e9f433f99b04a0317b3a8b7
Samuel Joel T <samueljoel464@gmail.com>
2024-03-14 16:53:38 +0700

    feat: finalizing catch feature and initializing my pokemon page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@
 import ReactDOM from 'react-dom/client'
 import App from '@/routes'
 import '@/styles/index.css'
+import { Toaster } from 'sonner';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <App />
-  </>,
-)
+    <Toaster />
+  </>
+);
--- src/pages/catch.tsx
+++ src/pages/catch.tsx
@@ -1,38 +1,26 @@
 import Layout from "@/components/layout";
 import { Button } from "@/components/ui/button";
+import CatchDialog from "@/components/catch-dialog";
 import { getPokemonDetails } from "@/utils/api-list/api";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { IDetail } from "@/utils/types/type";
 
-const Catch = () => {
-  const [counter, setCounter] = useState(0);
-  const { id } = useParams<{ id?: string }>();
-  const [pokemonDetail, setPokemonDetail] = useState<IDetail[]>([]);
 
-  const catchPokemon = (pokemonIdOrName: string) => {
-    try {
-      if (Math.random() < 0.5) {
-        setCounter((prevCounter) => prevCounter + 1);
-        localStorage.setItem(`pokemon${counter + 1}`, "Captured");
-        console.log(`Caught Pokemon: ${pokemonIdOrName}`);
-        toast("You caught one!");
-      } else {
-        console.log("You missed! Womp Womp");
-        toast("You missed! Womp Womp");
-      }
-    } catch (error) {
-      toast.error("Womp Womp");
-    }
-  };
+
+const Catch = () => {
+  const params = useParams();
+  const [pokemonDetail, setPokemonDetail] = useState<IDetail>();
+  const [showDialog, setShowDialog] = useState(false);
+   const [alias, setAlias] = useState<string>("");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-        const response = await getPokemonDetails(url);
-        setPokemonDetail([response]);
+        const response = await getPokemonDetails(params.name!);
+
+        setPokemonDetail(response);
       } catch (error) {
         toast((error as Error).message.toString());
       }
@@ -41,30 +29,70 @@ const Catch = () => {
     fetchData();
   }, []);
 
-
-  const setImageURL = (id: string) => {
-    const newLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
-    return newLink;
+  const catchPokemon = (name: string | undefined) => {
+    if (Math.random() < 0.5) {
+      toast(`You Caught ${name} !`);
+      setShowDialog(true);
+    } else {
+      toast("You missed! Womp Womp");
+    }
   };
 
+  const submitCaughtPokemon = () => {
+    const getFromLocal = JSON.parse(localStorage.getItem("myPokemons") || "[]");
+    const searchDupe = getFromLocal.find((x) => x.alias === alias);
+
+    if (searchDupe) {
+      alert(`Alias ${alias} is already exist!`);
+    } else {
+      const dupe = { ...pokemonDetail, alias };
+      getFromLocal.push(dupe);
+      localStorage.setItem("myPokemons", JSON.stringify(getFromLocal));
+
+      setShowDialog(false);
+    }
+  };
+
+
   return (
-    <div>
-      {pokemonDetail.map((pokemonDetail) => (
-        <Layout>
-          <div>
-            {pokemonDetail && id && <img src={setImageURL(id)} alt="" />}
-          </div>
-          <div className="grid justify-center">
-            <Button
-              className="self-center"
-              onClick={() => catchPokemon(pokemonDetail.name)}
-            >
-              Catch Pokemon
-            </Button>
-          </div>
-        </Layout>
-      ))}
-    </div>
+    <Layout>
+      <div className="grid justify-center">
+        <Button
+          className="self-center"
+          onClick={() => catchPokemon(pokemonDetail?.name)}
+        >
+          Catch Pokemon
+        </Button>
+      </div>
+      <CatchDialog show={showDialog}>
+        <div className="mb-5">
+          <p className="text-center font-arcade text-xs font-bold tracking-wide text-neutral-800 dark:text-white">
+            Congratulation!
+          </p>
+          <p className="text-center font-arcade text-xs font-bold tracking-wide text-neutral-800 dark:text-white">
+            You Caught {pokemonDetail?.name}
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <label className="block">
+            <span className="block font-arcade text-sm font-medium text-neutral-800 dark:text-white">
+              Nickname
+            </span>
+            <input
+              className="block w-full rounded-md border border-slate-300 bg-white py-2 px-3 font-arcade text-xs shadow-sm placeholder:italic focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500"
+              type="text"
+              onChange={(e) => setAlias(e.target.value)}
+            />
+          </label>
+          <button
+            className="mt-4 rounded-xl border p-3 text-center font-arcade text-xs tracking-wide text-neutral-800 dark:text-white"
+            onClick={() => submitCaughtPokemon()}
+          >
+            Submit
+          </button>
+        </div>
+      </CatchDialog>
+    </Layout>
   );
 };
 
