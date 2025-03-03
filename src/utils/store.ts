833a835247482d75aa0727e241172935b3e32b6c
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-03-03 15:42:17 +0700

    feat: extend Zustand store to include Pokémon details management

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,12 +1,20 @@
 import { create } from "zustand";
 import { IPokemon } from "@/utils/types/results.ts";
+import { IDetail } from "@/utils/types/type.ts";
+import { initialPokemonDetail } from "@/masterdata/initial-data.ts";
 
 type PokemonStore = {
   pokemonList: IPokemon[];
   setPokemonList: (pokemonList: IPokemon[]) => void;
+
+  pokemonDetails: IDetail;
+  setPokemonDetails: (pokemonDetails: IDetail) => void;
 };
 
 export const usePokemonStore = create<PokemonStore>((set) => ({
   pokemonList: [],
   setPokemonList: (pokemonList: IPokemon[]) => set({ pokemonList }),
+
+  pokemonDetails: initialPokemonDetail,
+  setPokemonDetails: (pokemonDetails: IDetail) => set({ pokemonDetails }),
 }));
