3d4db38199525aaa2c7a715f27290416a41748a9
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-03-03 15:42:04 +0700

    feat: add initial Pokémon detail data structure

---
+++ src/masterdata/initial-data.ts
@@ -0,0 +1,18 @@
+import { IDetail } from "@/utils/types/type.ts";
+
+export const initialPokemonDetail: IDetail = {
+  abilities: [],
+  alias: "",
+  id: 0,
+  moves: [],
+  name: "",
+  url: "",
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: "",
+      },
+    },
+  },
+  types: [],
+};
