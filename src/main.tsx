a122e4a1643b6df13f021d3eb63e54741c047d15
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-03-03 08:05:28 +0700

    feat: integrate React Buddy for component previews and add initial hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import ReactDOM from "react-dom/client";
 import App from "@/routes";
 import "@/styles/index.css";
 import "@/styles/layout.css";
-import { Toaster } from "@/components/ui/toaster";
+import {Toaster} from "@/components/ui/toaster";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "@/dev";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Toaster />
-  </>
+    <>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+        <Toaster/>
+    </>
 );
